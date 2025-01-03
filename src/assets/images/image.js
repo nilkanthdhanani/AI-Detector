f9caea97886473e8a6d4efce0c48932b17ea4cb1
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2025-01-03 12:55:23 +0530

    change image formate

--- src/assets/images/image.js
+++ src/assets/images/image.js
@@ -1,13 +1,13 @@
-import bannerEllipse from "./png/bannerEllipse.png"
-import whoUseImg1 from "./png/whoUseImg1.jpg"
-import whoUseImg2 from "./png/whoUseImg2.png"
-import whoUseImg3 from "./png/whoUseImg3.png"
-import whoUseImg4 from "./png/whoUseImg4.png"
-import clientImg from "./png/clientImg.png"
-import queriesImg from "./png/queriesImg.png"
-import blog1 from "./png/blog1.png"
-import blog2 from "./png/blog2.png"
-import blog3 from "./png/blog3.png"
+import bannerEllipse from "./webp/bannerEllipse.webp"
+import whoUseImg1 from "./webp/whoUseImg1.webp"
+import whoUseImg2 from "./webp/whoUseImg2.webp"
+import whoUseImg3 from "./webp/whoUseImg3.webp"
+import whoUseImg4 from "./webp/whoUseImg4.webp"
+import clientImg from "./webp/clientImg.webp"
+import queriesImg from "./webp/queriesImg.webp"
+import blog1 from "./webp/blog1.webp"
+import blog2 from "./webp/blog2.webp"
+import blog3 from "./webp/blog3.webp"
 
 export {
     bannerEllipse, whoUseImg1, whoUseImg2, whoUseImg3, whoUseImg4, clientImg, queriesImg, blog1, blog2, blog3
