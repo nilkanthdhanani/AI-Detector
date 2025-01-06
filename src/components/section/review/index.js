c0e588aea30c9ba73b4f3a5e702df3b1ceb25525
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2025-01-06 18:37:17 +0530

    sample

--- src/components/section/review/index.js
+++ src/components/section/review/index.js
@@ -28,7 +28,7 @@ export default function Review() {
                             </div>
                             <div className={styles.reviewListBoxText}>
                                 <QuotesIcon />
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 150s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                             </div>
                         </div>
                     ))}
